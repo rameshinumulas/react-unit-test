6ad827ce3d4e77047ab77488760cf60196cee802
rameshinumula <inumularamesh@gmail.com>
2024-04-24 22:29:25 +0530

    initial unit test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import FirstTest from './components/FirstTest';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('this is App component');
   expect(linkElement).toBeInTheDocument();
 });
+
+test('render first test component', () => {
+  render(<FirstTest />);
+  const linkElement = screen.getByText('this is first test');
+  expect(linkElement).toBeInTheDocument();
+})
